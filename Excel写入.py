be5943f0e77e8218e6e9ded7bd0dfe02fe100022
bgghue <2572563680@qq.com>
2021-08-27 10:36:49 +0800

    测试代码

--- Excel写入.py
+++ Excel写入.py
@@ -3,4 +3,8 @@ import xlwings as xw
 #测试
 app = xw.App(visible=True,add_book=False)
 wb = app.books.add()
-sht = xw.sheets["sheets1"]
+sht = xw.sheets["sheet1"]
+sht.range("A1").value = "ycb"
+wb.save("测试表.xlsx")
+wb.close
+app.quit
